68054f640b66297ca52b2669974fd89654f7fe06
Hristo Roque <aroquega@unsa.edu.pe>
2020-12-16 00:02:38 -0500

    Adds script and prefab that controls Scores

---
+++ Assets/Scripts/UI/ScoreView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreView : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI killCountText;
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+    private void Awake()
+    {
+        UpdateKillCount(0);
+        UpdateScore(0);
+    }
+    public void UpdateKillCount(int killCount)
+    {
+        killCountText.text = FormatNumber(
+            "Asesinatos", killCount
+        );
+    }
+    public void UpdateScore(int score)
+    {
+        scoreText.text = FormatNumber(
+            "Puntaje", score
+        );
+    }
+
+    private string FormatNumber(string text, int number)
+    {
+        return $"{text}: {number:0000}";
+    }
+}
